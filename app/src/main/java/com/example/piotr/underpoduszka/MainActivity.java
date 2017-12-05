f3a55163f15c14f973d4f2b8364552c825d80d72
Piotr <pietrucha865@gmail.com>
2017-12-05 22:42:11 +0100

    COMMIT WEKA KURCZE JCURKA

--- app/src/main/java/com/example/piotr/underpoduszka/MainActivity.java
+++ app/src/main/java/com/example/piotr/underpoduszka/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.piotr.underpoduszka;
 
 import android.content.Intent;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -18,11 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void setImage() {
         ImageView pillow = (ImageView) findViewById(R.id.pillow);
-       // pillow.setImage(R.drawable.pillow);
     }
 
     public void startMonitor(View view) {
         Intent intent = new Intent(this, MonitoringActivity.class);
+        startActivity(intent);
+    }
+
+    public void analyze(View view){
+        Intent intent = new Intent(this, AnalysisActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/piotr/underpoduszka/MonitoringActivity.java
+++ app/src/main/java/com/example/piotr/underpoduszka/MonitoringActivity.java
@@ -1,51 +1,63 @@
 package com.example.piotr.underpoduszka;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Locale;
-
 public class MonitoringActivity extends AppCompatActivity {
+    private final int Period = /*15 * 60 **/ 10000;
+    private RestService restService = new RestService();
+    private Handler handler = new Handler();
+    boolean stop = true;
     Sensor gyroscopeSensor;
     Sensor accelerometerSensor;
     SensorManager manager ;
-    TextView gyroscope;
-    TextView acelerometer;
-    float last_Update  = 0;
-    TextView microphone;
     Button button;
     SensorEventListener gyroscopeListener;
     SensorEventListener accelerometerListener;
+    Float accelerometerMeasurement = (float) 0;
+    Float gyroscopeMeasurement = (float) 0;
+    String UserName;
+    String City;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitoring);
-        gyroscope = (TextView) findViewById(R.id.gyroscope);
-        acelerometer = (TextView) findViewById(R.id.akcelometer);
-        microphone = (TextView) findViewById(R.id.microphone);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_input, null);
+        builder.setView(dialogView);
+        final EditText name = (EditText) dialogView.findViewById(R.id.username);
+        final EditText city = (EditText) dialogView.findViewById(R.id.city);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                City = city.getText().toString();
+                UserName = name.getText().toString();
+            }
+        }).create().show();
     }
 
     private void GyroscopeListener() {
         gyroscopeSensor = manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-
         gyroscopeListener = new SensorEventListener() {
+
             @Override
             public void onSensorChanged(SensorEvent event) {
-                float x = event.values[0];
-                float y = event.values[1];
-                String t1 = String.format(Locale.getDefault(),"%.2f",x);
-                String t2 = String.format(Locale.getDefault(),"%.2f",y);
-                gyroscope.setText(t1+"\n"+t2+"\n");
+
+                if((event.values[0] + event.values[1] + event.values[2]) / 3 > accelerometerMeasurement)
+                    gyroscopeMeasurement = (event.values[0] + event.values[1] + event.values[2]) / 3;
             }
 
             @Override
@@ -56,58 +68,13 @@ public class MonitoringActivity extends AppCompatActivity {
         manager.registerListener(gyroscopeListener,gyroscopeSensor, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
-    public void StartStop(View view) {
-        button = (Button) findViewById(R.id.startstop);
-        if(button.getText() == "STOP"){
-            button.setText("START");
-            manager.unregisterListener(gyroscopeListener, gyroscopeSensor);
-        }
-        else {
-            manager = (SensorManager) getSystemService(SENSOR_SERVICE);
-            button.setText("STOP");
-            GyroscopeListener();
-            AcelerometerListener();
-            //MicrophoneListener();
-        }
-    }
-
-    private void MicrophoneListener() {
-        accelerometerSensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        gyroscopeListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-
-                Toast.makeText(getApplicationContext(), "sadsa", Toast.LENGTH_SHORT).show();
-                float x = event.values[0];
-                float y = event.values[1];
-                float z = event.values[3];
-
-
-
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-        };
-        manager.registerListener(gyroscopeListener,gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
     private void AcelerometerListener() {
         accelerometerSensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-
         accelerometerListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                float x = event.values[0];
-                float y = event.values[1];
-                float z = event.values[2];
-                String t1 = String.format(Locale.getDefault(),"%.2f",x);
-                String t2 = String.format(Locale.getDefault(),"%.2f",y);
-                String t3 = String.format(Locale.getDefault(),"%.2f",z);
-                acelerometer.setText(t1+"\n"+t2+"\n"+t3);
+                    if((event.values[0] + event.values[1] + event.values[2] - 9.8) / 3 > accelerometerMeasurement)
+                    accelerometerMeasurement = (float) (event.values[0] + event.values[1] + event.values[2] - 9.8) / 3;
             }
 
             @Override
@@ -117,4 +84,72 @@ public class MonitoringActivity extends AppCompatActivity {
         };
         manager.registerListener(accelerometerListener,accelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+
+    }
+
+    public void StartStop(View view) {
+        button = (Button) findViewById(R.id.startstop);
+        if(button.getText() == "STOP"){
+            stop = true;
+            button.setText("START");
+            manager.unregisterListener(gyroscopeListener, gyroscopeSensor);
+            manager.unregisterListener(accelerometerListener, accelerometerSensor);
+        }
+        else {
+            stop = false;
+            manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            button.setText("STOP");
+            GyroscopeListener();
+            AcelerometerListener();
+            SendPackagesEvery15minutes();
+
+        }
+    }
+
+    private void SendPackagesEvery15minutes() {
+    handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            if(!stop)
+            ProcessData();
+            handler.postDelayed(this, Period);
+        }
+    }, Period);
+    }
+
+    private void ProcessData() {
+        manager.unregisterListener(gyroscopeListener, gyroscopeSensor);
+        manager.unregisterListener(accelerometerListener, accelerometerSensor);
+        DataModel model = new DataModel(String.valueOf(gyroscopeMeasurement), String.valueOf(accelerometerMeasurement), UserName, City);
+        Toast.makeText(getApplicationContext(), "Przesyłam dane...", Toast.LENGTH_SHORT).show();
+        new ProcessDataToServer(model).execute();
+        gyroscopeMeasurement = (float)0;
+        accelerometerMeasurement = (float)0;
+        manager.registerListener(gyroscopeListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        manager.registerListener(accelerometerListener, accelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+
+
+    public void Back(View view) {
+        AlertDialog.Builder wantBack = new AlertDialog.Builder(MonitoringActivity.this).setMessage("Chcesz zakończyć monitoring?");
+        wantBack.setCancelable(false).setPositiveButton("TAK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               finish();
+            }
+        }).setNegativeButton("NIE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        }).show();
+    }
+
+
 }
